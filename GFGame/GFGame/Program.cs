23eec7dc08467bab45d2804e4b33f35c0fb78eef
Xena92 <Ida.thuesen@hotmail.com>
2014-04-27 01:28:29 +0200

    ALT

--- GFGame/GFGame/Program.cs
+++ GFGame/GFGame/Program.cs
@@ -10,6 +10,9 @@ namespace Example {
         [STAThread]
         public static void Main() {
 
+            double beginX = 300.0, beginY = 300.0, QHeight = 50.0, QWidth = 50.0;
+            int acceleration = 1, accelerationCount = 0, accelerationCountTarget = 10;
+
             using (GameWindow game = new GameWindow()) {
 
                 // Always start by loading the things needed for the game.
@@ -17,6 +20,10 @@ namespace Example {
                     // setup settings, load textures, sounds
                     GL.MatrixMode(MatrixMode.Projection);
                     GL.LoadIdentity();
+                    game.Width = 1024;
+                    game.Height = 768;
+                    game.X = 400;
+                    game.Y = 200;
                     GL.Ortho(0.0, (double)game.Width, (double)game.Height, 0.0, 0.0, 4.0);
                     //       left,       right,             bottom,       top, zNear, zFar  <-- all double
                     game.VSync = VSyncMode.On;
@@ -34,12 +41,60 @@ namespace Example {
                     if (game.Keyboard[Key.Escape]) {
                         game.Exit();
                     }
+
+                    if (game.Keyboard[Key.Down]) {
+                        beginY+= acceleration;
+                    }
+                    else if (game.Keyboard[Key.Up]) {
+                        beginY-= acceleration;
+                    }
+
+                    if (game.Keyboard[Key.Right]) {
+                        beginX+= acceleration;
+                        accelerationCount++;
+                        if (accelerationCount == accelerationCountTarget) {
+                            acceleration++;
+                            accelerationCount = 0;
+                        }
+                    }
+                    else if (game.Keyboard[Key.Left]) {
+                        beginX-= acceleration;
+                        accelerationCount++;
+                        if (accelerationCount == accelerationCountTarget) {
+                            acceleration++;
+                            accelerationCount = 0;
+                        }
+                    }
+                    else {
+                        accelerationCount = 0;
+                        acceleration = 1;
+                    }
+
+                    if (game.Keyboard[Key.K]) {
+                        beginX = 300.0;
+                        beginY = 300.0;
+                    }
                 };
 
                 game.RenderFrame += (sender, e) => {
                     // render graphics
+                    GL.ClearColor(0.1f, 0.8f, 1.0f, 1.0f);
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                    GL.Begin(PrimitiveType.Quads);
 
+                    GL.Color3(Color.Cyan);
+                    GL.Vertex2(beginX, QHeight + beginY);
+
+                    GL.Color3(Color.Black);
+                    GL.Vertex2(beginX, beginY);
+
+                    GL.Color3(Color.Green);
+                    GL.Vertex2(QWidth + beginX, beginY);
+
+                    GL.Color3(Color.Red);
+                    GL.Vertex2(QWidth + beginX, QHeight + beginY);
+
+                    GL.End();
                     //Draw stuff here
 
                     game.SwapBuffers();
