61a7bbea01d26d889f42f2fa97b6dfa9466ed412
Junker91 <sashiba91@gmail.com>
2014-04-26 20:05:03 +0200

    GameWindow class(working)

--- GFGame/GFGame/Program.cs
+++ GFGame/GFGame/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+//using OpenTK.Graphics;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
-namespace GFGame {
-    class Program {
-        static void Main(string[] args) {
+namespace Example {
+    class MyApplication {
+        [STAThread]
+        public static void Main() {
+
+            using (GameWindow game = new GameWindow()) {
+
+                // Always start by loading the things needed for the game.
+                game.Load += (sender, e) => {
+                    // setup settings, load textures, sounds
+                    GL.MatrixMode(MatrixMode.Projection);
+                    GL.LoadIdentity();
+                    GL.Ortho(0.0, (double)game.Width, (double)game.Height, 0.0, 0.0, 4.0);
+                    //       left,       right,             bottom,       top, zNear, zFar  <-- all double
+                    game.VSync = VSyncMode.On;
+
+                    game.WindowBorder = WindowBorder.Fixed;
+                };
+
+                //Resize
+                game.Resize += (sender, e) => {
+                    GL.Viewport(0, 0, game.Width, game.Height);
+                };
+
+                game.UpdateFrame += (sender, e) => {
+                    // add game logic, input handling
+                    if (game.Keyboard[Key.Escape]) {
+                        game.Exit();
+                    }
+                };
+
+                game.RenderFrame += (sender, e) => {
+                    // render graphics
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    //Draw stuff here
+
+                    game.SwapBuffers();
+                };
+
+                // Run the game at 60 updates per second
+                game.Run(60.0);
+            }
         }
     }
 }
